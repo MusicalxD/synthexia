283f6c5b02a51244025c01422c71144d2d219e1f
MusicalxD <mohamedwaleed9990@gmail.com>
2023-08-12 12:45:48 +0300

    added a preloader screen

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,9 @@
 document.querySelector(".nav-icon").addEventListener("click", function () {
   document.querySelector("nav").classList.toggle("show-nav");
 });
+
+// preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
