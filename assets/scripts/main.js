0e8d10d6ff011262ccae607f7510ce2b7f0784a0
MusicalxD <mohamedwaleed9990@gmail.com>
2023-08-17 18:52:15 +0300

    1 - added a new animation for the menu icon 2 - made few xhanges and fixes for fonts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,11 @@
-// nav icon
-document.querySelector(".nav-icon").addEventListener("click", function () {
-  document.querySelector("nav").classList.toggle("show-nav");
-});
-
 // preloader
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   loader.style.display = "none";
+});
+
+// nav icon
+document.querySelector(".nav-icon").addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("show-nav");
+  document.querySelector(".nav-icon").classList.toggle("rotate");
 });
